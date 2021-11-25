77725a760666ce7b4630d7f5fe83210c19c11052
John Garzon <john.garzon.ex@copec.cl>
2021-11-25 14:54:57 -0500

    Se crea la tabla y ademas se crean las alertas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
+import { HeroesComponent } from './pages/heroes/heroes.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroeComponent } from './pages/heroe/heroe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroe', component: HeroeComponent },
+  { path: 'heroe/:id', component: HeroeComponent },
+  { path: 'heroes', component: HeroesComponent },
+  { path: '**', redirectTo: 'heroes', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
